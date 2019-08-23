c61a8a8916bb34da0e9fd27641490ac0f40fc5cc
James Knighton <iamknighton@gmail.com>
2019-08-22 18:29:47 -0700

    Hook up login/reg pages.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
+var session = require('express-session');
 
 var key = fs.readFileSync('pem/key.pem');
 var cert = fs.readFileSync('pem/cert.pem');
@@ -14,9 +15,6 @@ var options = {
 };
 
 var app = express();
-https.createServer(options, app).listen(443);
-
-http.createServer(app).listen(80);
 
 app.use(function(req, res, next) {
     if (req.secure) {
@@ -26,6 +24,64 @@ app.use(function(req, res, next) {
     }
 });
 
+app.use(session({
+    secret: 'big-kahuna-burger',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get('/', function(req, res) {
-    res.send('Hello, world!');
+    if (req.session.user) {
+        fs.readFile('src/play.html', 'utf8', function(err, data) {
+            if (err) {
+                res.send('');
+                return;
+            }
+            res.send(data);
+        });
+    } else {
+        fs.readFile('src/index.html', 'utf8', function(err, data) {
+            if (err) {
+                res.send('');
+                return;
+            }
+            res.send(data);
+        });
+    }
 });
+
+app.get('/login', function(req, res) {
+    if (req.session.user) {
+        res.redirect('/');
+        return;
+    }
+
+    fs.readFile('src/login.html', 'utf8', function(err, data) {
+        if (err) {
+            res.send('');
+            return;
+        }
+        res.send(data);
+    });
+});
+
+app.get('/register', function(req, res) {
+    if (req.session.user) {
+        res.redirect('/');
+        return;
+    }
+
+    fs.readFile('src/register.html', 'utf8', function(err, data) {
+        if (err) {
+            res.send('');
+            return;
+        }
+        res.send(data);
+    });
+});
+
+https.createServer(options, app).listen(443);
+http.createServer(app).listen(80);
