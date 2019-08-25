ee0153d1cd08cad5f3e7b84a77aa32307105eba1
James Knighton <iamknighton@gmail.com>
2019-08-25 01:15:39 -0700

    Switch to sqlite3.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-var bcrypt = require('bcrypt');
+var crypto = require('crypto');
 var express = require('express');
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var session = require('express-session');
+var sqlite3 = require('sqlite3').verbose();
 
 var key = fs.readFileSync('pem/key.pem');
 var cert = fs.readFileSync('pem/cert.pem');
@@ -38,44 +39,29 @@ if (!fs.existsSync('data/')) {
     fs.mkdirSync('data/');
 }
 
-var load_user_db = function(filename) {
-    if (!fs.existsSync(filename)) {
-        return [];
-    }
-    var user_db = [];
-    var lines = fs.readFileSync(filename).toString().split('\n');
-    for (var i = 0; i < lines.length; ++i) {
-        var line = lines[i];
-        var user = JSON.parse(line);
-        user_db.push(user);
-    }
-    return user_db;
-};
+var db = new sqlite3.Database('data/db.sqlite3');
 
-var save_user_db = function(user_db, filename) {
-    if (fs.existsSync(filename)) {
-        fs.unlinkSync(filename);
-    }
-    var lines = [];
-    for (var i = 0; i < user_db.length; ++i) {
-        var user = user_db[i];
-        var line = JSON.stringify(user);
-        lines.push(line);
-    }
-    fs.writeFileSync(filename, lines.join('\n'));
-};
+db.serialize(function() {
+    db.run('CREATE TABLE IF NOT EXISTS users (' +
+        'id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+        'username TEXT NOT NULL, ' +
+        'password_hash TEXT NOT NULL, ' +
+        'salt TEXT NOT NULL, ' +
+        'created REAL NOT NULL, ' +
+        'initial_elo REAL NOT NULL, ' +
+        'elo REAL NOT NULL, ' +
+        'email TEXT)');
 
-var user_db = load_user_db('data/users.jsonl');
-
-var get_user_by_username = function(username) {
-    for (var i = 0; i < user_db.length; ++i) {
-        var user = user_db[i];
-        if (user.username == username) {
-            return user;
-        }
-    }
-    return null;
-};
+    db.run('CREATE TABLE IF NOT EXISTS games (' +
+        'id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+        'lower_uid INTEGER NOT NULL, ' +
+        'higher_uid INTEGER NOT NULL, ' +
+        'begin REAL NOT NULL, ' +
+        'end REAL, ' +
+        'winner INTEGER, ' +
+        'loser INTEGER, ' +
+        'data TEXT NOT NULL)');
+});
 
 // -----------------------------------------------------------------------------
 
@@ -151,6 +137,7 @@ app.get('/play/:vs', function(req, res) {
     }
 
     fs.readFile('src/play.html', 'utf8', function(err, data) {
+        data = data.replace('{{vs}}', vs);
         res.send(data);
     });
 });
@@ -168,30 +155,101 @@ var make_ok = function() {
     return make_error(null);
 };
 
+var is_username_valid = function(s) {
+    if (!s) {
+        return false;
+    }
+
+    if (20 < s) {
+        return false;
+    }
+
+    for (var i = 0; i < s.length; ++i) {
+        var c = s[i];
+        if ('a' <= c && c <= 'z') {
+            continue;
+        }
+        if ('A' <= c && c <= 'Z') {
+            continue;
+        }
+        if ('0' <= c && c <= '9') {
+            continue;
+        }
+        return false;
+    }
+
+    return true;
+};
+
+var hash_password = function(password, salt) {
+    var x = crypto.createHash('sha256');
+    x.update(password);
+    x.update(salt);
+    return x.digest('hex');
+};
+
 app.post('/api/register', function(req, res) {
     if (is_logged_in(req)) {
         res.send(make_error('already_logged_in'));
         return;
     }
 
-    for (var i = 0; i < user_db.length; ++i) {
-        var user = user_db[i];
-        if (req.body.username == user.username) {
+    if (!req.body.username) {
+        res.send(make_error('username_dne'));
+        return;
+    }
+
+    if (!is_username_valid(req.body.username)) {
+        res.send(make_error('bad_username'));
+        return;
+    }
+
+    if (!req.body.password) {
+        res.send(make_error('password_dne'));
+        return;
+    }
+
+    if (!req.body.elo) {
+        res.send(make_error('elo_dne'));
+        return;
+    }
+
+    var elo = parseInt(req.body.elo);
+    if (!elo) {
+        res.send(make_error('bad_elo'));
+        return;
+    }
+
+    var sql = 'SELECT id FROM users WHERE username=?';
+    var params = [req.body.username];
+    db.get(sql, params, function(err, row) {
+        if (row) {
             res.send(make_error('username_is_taken'));
             return;
         }
-    }
 
-    var id = user_db.length;
-    var user = {
-        id: id,
-        username: req.body.username,
-        password: bcrypt.hashSync(req.body.password, 10),
-    };
-    user_db.push(user);
-    save_user_db(user_db, 'data/users.jsonl');
-    req.session.username = user.username;
-    res.send(make_ok());
+        var salt = Math.floor(Math.random() * 0xFFFFFFFF).toString(16);
+        var password_hash = hash_password(req.body.password, salt);
+        var created = (new Date).getTime() / 1000;
+
+        var sql = 'INSERT INTO users (username, password_hash, salt, ' +
+            'created, initial_elo, elo, email) VALUES (?, ?, ?, ?, ?, ?, ' +
+            '?)';
+        var params = [
+            req.body.username,
+            password_hash,
+            salt,
+            created,
+            elo,
+            elo,
+            req.body.email,
+        ];
+        db.run(sql, params, function(err, ret) {
+            req.session.uid = this.lastID;
+            req.session.username = req.body.username;
+            res.send(make_ok());
+        });
+    });
 });
 
 app.post('/api/login', function(req, res) {
@@ -200,17 +258,39 @@ app.post('/api/login', function(req, res) {
         return;
     }
 
-    for (var i = 0; i < user_db.length; ++i) {
-        var user = user_db[i];
-        if (req.body.username == user.username &&
-                bcrypt.compareSync(req.body.password, user.password)) {
-            req.session.username = user.username;
-            res.send(make_ok());
-            return;
-        }
+    if (!req.body.username) {
+        res.send(make_error('username_dne'));
+        return;
     }
 
-    res.send(make_error('bad'));
+    if (!is_username_valid(req.body.username)) {
+        res.send(make_error('bad_username'));
+        return;
+    }
+
+    if (!req.body.password) {
+        res.send(make_error('password_dne'));
+        return;
+    }
+
+    var sql = 'SELECT id, password_hash, salt FROM users WHERE username=?';
+    var params = [req.body.username];
+    db.get(sql, params, function(err, row) {
+        if (!row) {
+            res.send(make_error('user_dne'));
+            return;
+        }
+
+        var password_hash = hash_password(req.body.password, row.salt);
+        if (password_hash != row.password_hash) {
+            res.send(make_error('wrong_password'));
+            return;
+        }
+
+        req.session.uid = row.id;
+        req.session.username = req.body.username;
+        res.send(make_ok());
+    });
 });
 
 app.post('/api/logout', function(req, res) {
@@ -224,20 +304,25 @@ app.post('/api/logout', function(req, res) {
 });
 
 app.post('/api/get_users', function(req, res) {
-    var rr = [];
-    for (var i = 0; i < user_db.length; ++i) {
-        var user = user_db[i];
+    var sql = 'SELECT username, elo FROM users ORDER BY elo DESC';
+    var params = [];
+    db.all(sql, params, function(err, rows) {
+        var users = [];
+        for (var i = 0; i < rows.length; ++i) {
+            var row = rows[i];
+            var user = {
+                username: row.username,
+                elo: row.elo
+            };
+            users.push(user);
+        }
         var r = {
-            id: user.id,
-            username: user.username,
+            error: null,
+            users: users,
         };
-        rr.push(r);
-    }
-    var r = {
-        error: null,
-        users: rr,
-    };
-    res.send(JSON.stringify(r));
+        var s = JSON.stringify(r);
+        res.send(s);
+    });
 });
 
 // -----------------------------------------------------------------------------
